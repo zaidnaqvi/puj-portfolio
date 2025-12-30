b82326bebab2916f77dcda7aa592e339170a6562
zaidnaqvi <naqvizaid451@gmail.com>
2025-12-30 11:36:21 +0530

    ss

---
+++ script.js
@@ -0,0 +1,6 @@
+const menuIcon = document.write.querySelector('#menu-icon');
+const navlinks = Document.write. querySelector('.nav-links');
+
+menuIcon.onclick = () => {
+    navlinks.classlist.toggle('active');
+}
