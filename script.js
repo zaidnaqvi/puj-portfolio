4608f640db10d6790f640e86d65ea8390cb1bdd1
zaidnaqvi <naqvizaid451@gmail.com>
2025-12-30 12:43:31 +0530

    ss

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 const menuBtn = document.getElementById("menu-btn");
 const nav = document.querySelector(".nav");
 
-menuBtn.addEventListener("click", () => {
+menuBtn.onclick = () => {
   nav.classList.toggle("active");
-});
+};
+
+/* SCROLL REVEAL */
+const reveals = document.querySelectorAll(".reveal");
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("active");
+      }
+    });
+  },
+  { threshold: 0.15 }
+);
+
+reveals.forEach((r) => observer.observe(r));
